136460653f735143b5b072250b52685b835d095f
Markus Erdinger <mkerdinger@freenet.de>
2022-05-16 10:04:04 +0200

    eine Commit Message

---
+++ src/Hello.java
@@ -0,0 +1,11 @@
+public class Hello {
+
+    public static void main(String[] args) {
+        Hello h = new Hello();
+        h.grussErdinger();
+    }
+
+    public void grussErdinger(){
+        System.out.println("Hallo Kurs 5 inf!");
+    }
+}
